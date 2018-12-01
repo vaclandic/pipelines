77f5673bf5f183090f92737428cfb8072ed499a9
vk <vk@VAK.local>
2018-12-01 16:08:52 +0300

    fixed

--- src/configs/infra/deploy.groovy
+++ src/configs/infra/deploy.groovy
@@ -1,6 +1,8 @@
 package configs.infra
 
 class DeployStructure {
+
+  public static final String
   def env_str = [
         dev: [
           deploy_servers: "dev2"
@@ -12,4 +14,7 @@ class DeployStructure {
           deploy_servers: "test2"
         ]
       ]
+    public static final String server = "Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'"    def rtMaven = Artifactory.newMavenBuild()
+    public static final String rtMaven = "Artifactory.newMavenBuild()"
+    public static final string docker_reg = "docker.trph.ru"
 }
