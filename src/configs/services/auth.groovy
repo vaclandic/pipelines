baf4b3b769b1ab45ad44b97ae5ccd3028ee154a0
vk <vk@VAK.local>
2018-12-01 15:16:31 +0300

    fixed

--- src/configs/services/auth.groovy
+++ src/configs/services/auth.groovy
@@ -1,5 +1,19 @@
 package configs.services
 
-class Config {
-  public static final String PORT = '8443'
+class AuthConfig {
+  public static final String NAME       = 'srv-auth';
+  public static final String PORT       = '8443';
+  public static final String DEBUG_PORT = '18284';
+
+  def  env_str = [
+        dev: [
+          deploy_servers: "dev2"
+        ],
+        test: [
+          deploy_servers: "test2"
+        ],
+        prod: [
+          deploy_servers: "test2"
+        ]
+      ]
 }
