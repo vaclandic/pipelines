59b49ffd3315f03c1f1521c488aafb8ad72b285f
vk <vk@VAK.local>
2018-12-01 16:12:52 +0300

    fixed

--- src/configs/infra/deploy.groovy
+++ src/configs/infra/deploy.groovy
@@ -16,5 +16,5 @@ class DeployStructure {
       ]
     public static final String server = "Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'"
     public static final String rtMaven = "Artifactory.newMavenBuild()"
-    public static final string docker_reg = "docker.trph.ru"
+    public static final String docker_reg = "docker.trph.ru"
 }
