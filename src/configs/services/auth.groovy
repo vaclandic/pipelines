0d73da274cb34af627964890948eedea2353d7aa
vk <vk@VAK.local>
2018-12-01 15:40:22 +0300

    fixed

--- src/configs/services/auth.groovy
+++ src/configs/services/auth.groovy
@@ -4,16 +4,4 @@ class AuthConfig {
   public static final String NAME       = 'srv-auth';
   public static final String PORT       = '8443';
   public static final String DEBUG_PORT = '18284';
-
-  def  env_str = [
-        dev: [
-          deploy_servers: "dev2"
-        ],
-        test: [
-          deploy_servers: "test2"
-        ],
-        prod: [
-          deploy_servers: "test2"
-        ]
-      ]
 }
