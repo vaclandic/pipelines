a4ce0fd1be77dc1fdfb48c73d06a8e60ad6bf0f2
vk <vk@VAK.local>
2018-12-01 16:09:37 +0300

    fixed

--- src/configs/services/auth.groovy
+++ src/configs/services/auth.groovy
@@ -4,23 +4,5 @@ class AuthConfig {
   public static final String NAME       = 'srv-auth';
   public static final String PORT       = '8443';
   public static final String DEBUG_PORT = '18284';
-  public static final StrinG DEBUG_OPTS = "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:DEBUG_PORT,suspend=n"
-
-  if (environment == "prod"){
-      env.STAGE = 'prerelease'
-      env.CPU_LIMIT = 200
-      env.MEMORY_LIMIT = 1024
-      env.JOLOKIA_PORT = 8087
-     // env.JAVA_OPTIONS = "-Xmx512m"
-      env.APP_VERSION = "release-${project.version.text()}"
-  }
-  else {
-      env.STAGE = environment
-      env.CPU_LIMIT = 200
-      env.MEMORY_LIMIT = 1024
-      env.JOLOKIA_PORT = 8087
-     // env.JAVA_OPTIONS = "-Xmx512m"
-      env.APP_VERSION = "${project.version.text()}"
-  }
-
+  public static final StrinG DEBUG_OPTS = "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:DEBUG_PORT,suspend=n";
 }
