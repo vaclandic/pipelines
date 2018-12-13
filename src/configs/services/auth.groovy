b1bf811028420b11c1b5703afa7e566dca3e5e9f
vk <vk@VAK.local>
2018-12-13 16:10:33 +0300

    fixed

--- src/configs/services/auth.groovy
+++ src/configs/services/auth.groovy
@@ -4,6 +4,4 @@ class Auth {
   public static final String NAME       = 'srv-auth';
   public static final String PORT       = '8443';
   public static final String DEBUG_OPTS = "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:18284,suspend=n";
-
-  return this
 }
--- src/configs/steps/build.groovy
+++ src/configs/steps/build.groovy
@@ -1,40 +1,37 @@
 package config.steps
 
-class Build implements Serializable {
-    def checkoutFrom(repo, branch) {
-        git([url: "git@bitbucket.org:TracePharm/${repo}", branch: "${branch}"])
+def checkoutFrom(repo, branch) {
+    git([url: "git@bitbucket.org:TracePharm/${repo}", branch: "${branch}"])
+}
+
+def buildAndPush(java_version="10", serviceName) {
+    def server = Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'
+    def rtMaven = Artifactory.newMavenBuild()
+    env.JAVA_HOME="/jdk${java_version}"
+
+    rtMaven.tool = "maven"
+    buildInfo = Artifactory.newBuildInfo()
+    rtMaven.deployer server: server, releaseRepo: 'artifactory', snapshotRepo: 'artifactory'
+
+    rtMaven.run pom: "${serviceName}/pom.xml" as String, goals: "-U clean install -Dmaven.test.skip=true -s /home/jenkins/settings.xml", buildInfo: buildInfo
+    server.publishBuildInfo buildInfo
+    rtMaven.deployer.deployArtifacts buildInfo
+}
+
+def dockerBuild(String serviceName, String workspace) {
+    servicePom = new XmlSlurper().parse(new File("${workspace}/${serviceName}/pom.xml"))
+    serviceVersion = servicePom.version
+
+      steps.sh "docker build --pull --build-arg SERVICE_NAME=${serviceName} -t docker.trph.ru/${serviceName}:${serviceVersion} ${serviceName}"
+}
+//    sh "docker push docker.trph.ru/${serviceName}:${serviceVersion}"
+
+def setEnv(workspace, serviceName, deployHost) {
+    def project = new XmlSlurper().parse(new File("${workspace}/${serviceName}/pom.xml"))
+    env.SERVICE = serviceName
+    env.HOST_NAME = deployHost
+
+    if (debug == "true"){
+       env.DEBUG_OPTS = service.DEBUG_OPTS
     }
-    
-    def buildAndPush(java_version="10", serviceName) {
-        def server = Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'
-        def rtMaven = Artifactory.newMavenBuild()
-        env.JAVA_HOME="/jdk${java_version}"
-    
-        rtMaven.tool = "maven"
-        buildInfo = Artifactory.newBuildInfo()
-        rtMaven.deployer server: server, releaseRepo: 'artifactory', snapshotRepo: 'artifactory'
-    
-        rtMaven.run pom: "${serviceName}/pom.xml" as String, goals: "-U clean install -Dmaven.test.skip=true -s /home/jenkins/settings.xml", buildInfo: buildInfo
-        server.publishBuildInfo buildInfo
-        rtMaven.deployer.deployArtifacts buildInfo
-    }
-    
-    def dockerBuild(String serviceName, String workspace) {
-        servicePom = new XmlSlurper().parse(new File("${workspace}/${serviceName}/pom.xml"))
-        serviceVersion = servicePom.version
-    
-//        steps.sh "docker build --pull --build-arg SERVICE_NAME=${serviceName} -t docker.trph.ru/${serviceName}:${serviceVersion} ${serviceName}"
-    }
-    //    sh "docker push docker.trph.ru/${serviceName}:${serviceVersion}"
-    
-    def setEnv(workspace, serviceName, deployHost) {
-        def project = new XmlSlurper().parse(new File("${workspace}/${serviceName}/pom.xml"))
-        env.SERVICE = serviceName
-        env.HOST_NAME = deployHost
-    
-        if (debug == "true"){
-           env.DEBUG_OPTS = service.DEBUG_OPTS
-        }
-    }
-return this
 }
