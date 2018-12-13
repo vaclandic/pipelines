bb804c91a606d92e1741f74dce5549509572cb60
vk <vk@VAK.local>
2018-12-13 11:34:16 +0300

    fixed

--- src/configs/steps/build.groovy
+++ src/configs/steps/build.groovy
@@ -1,7 +1,7 @@
 package config.steps
 
-def checkoutFrom() {
-    git([url: "git@bitbucket.org:TracePharm/tracepharmservices.git", branch: "master"])
+def checkoutFrom(repo, branch) {
+    git([url: "git@bitbucket.org:TracePharm/${repo}", branch: "${branch}"])
 }
 return this
 
@@ -11,5 +11,22 @@ def artifactoryConfig() {
     rtMaven.tool = "maven"
     buildInfo = Artifactory.newBuildInfo()
     rtMaven.deployer server: server, releaseRepo: 'artifactory', snapshotRepo: 'artifactory'
+}
+return this
+
+def setEnv(workspace, serviceName, deployHost, debug, ) {
+    def project = new XmlSlurper().parse(new File("${workspace}/${serviceName}/pom.xml"))
+    env.SERVICE = serviceName
+    env.HOST_NAME = deployHost
+
+    if (debug == "true"){
+       env.DEBUG_OPTS = service.DEBUG_OPTS
+    }
+}
+return this
+
+def mvnBuild(java_version=10) {
+    env.JAVA_HOME="/jdk${jdk}"
+    rtMaven.run pom: "service.NAME/pom.xml" as String, goals: "-U clean install -Dmaven.test.skip=true -s /home/jenkins/settings.xml", buildInfo: buildInfo
 }
 return this
