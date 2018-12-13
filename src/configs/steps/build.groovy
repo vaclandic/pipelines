0d619e919bb5b62994ed53f4f92341e9cef71e69
vk <vk@VAK.local>
2018-12-13 13:04:49 +0300

    fixed

--- src/configs/steps/build.groovy
+++ src/configs/steps/build.groovy
@@ -6,15 +6,6 @@ def checkoutFrom(repo, branch) {
     return this
 }
 
-//def artifactoryConfig() {
-//    def server = Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'
-//    def rtMaven = Artifactory.newMavenBuild()
-//    def rtMaven.tool = "maven"
-//    def buildInfo = Artifactory.newBuildInfo()
-//    rtMaven.deployer server: server, releaseRepo: 'artifactory', snapshotRepo: 'artifactory'
-//    return this
-//}
-
 def mvnBuild(java_version="10", serviceName) {
     def server = Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'
     def rtMaven = Artifactory.newMavenBuild()
@@ -31,10 +22,12 @@ def mvnBuild(java_version="10", serviceName) {
 def pullArtifact() {
     def server = Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'
     def rtMaven = Artifactory.newMavenBuild()
+
+    rtMaven.tool = "maven"
     buildInfo = Artifactory.newBuildInfo()
     rtMaven.deployer server: server, releaseRepo: 'artifactory', snapshotRepo: 'artifactory'
-    server.publishBuildInfo buildInfo
 
+    server.publishBuildInfo buildInfo
     rtMaven.deployer.deployArtifacts buildInfo
     return this
 }
