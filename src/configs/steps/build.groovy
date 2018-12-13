b5e3a615e193bc0882ad66df85db0069454e0b85
vk <vk@VAK.local>
2018-12-13 14:14:04 +0300

    fixed

--- src/configs/steps/build.groovy
+++ src/configs/steps/build.groovy
@@ -23,7 +23,7 @@ def buildAndPush(java_version="10", serviceName) {
 def dockerBuild(serviceName, workspace) {
     def servicePom = new XmlSlurper().parse(new File("${workspace}/${serviceName}/pom.xml"))
     def serviceVersion = servicePom.version.text()
-    script.sh "echo Good"
+    sh "echo Good"
 
 //    sh "docker build --pull --build-arg SERVICE_NAME=${serviceName} -t decker.trph.ru/${serviceName}:${serviceVersion} ${serviceName}"
 //    sh "docker push docker.trph.ru/${serviceName}:${serviceVersion}"
