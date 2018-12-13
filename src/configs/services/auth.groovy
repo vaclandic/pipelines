3b881c15df19aac8dccadde504397b180ced8679
vk <vk@VAK.local>
2018-12-13 10:23:27 +0300

    fixed

--- src/configs/services/auth.groovy
+++ src/configs/services/auth.groovy
@@ -1,6 +1,6 @@
 package configs.services
 
-class AuthConfig {
+class Auth {
   public static final String NAME       = 'srv-auth';
   public static final String PORT       = '8443';
   public static final String DEBUG_OPTS = "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:18284,suspend=n";
