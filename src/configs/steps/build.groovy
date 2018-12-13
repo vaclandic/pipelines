e319d3bc5274a77ec28cb70a6e1c0e06ec1db56d
vk <vk@VAK.local>
2018-12-13 12:57:58 +0300

    fixed

--- src/configs/steps/build.groovy
+++ src/configs/steps/build.groovy
@@ -1,5 +1,6 @@
 package config.steps
 
+
 def checkoutFrom(repo, branch) {
     git([url: "git@bitbucket.org:TracePharm/${repo}", branch: "${branch}"])
     return this
@@ -14,17 +15,6 @@ def checkoutFrom(repo, branch) {
 //    return this
 //}
 
-def setEnv(workspace, serviceName, deployHost) {
-    def project = new XmlSlurper().parse(new File("${workspace}/${serviceName}/pom.xml"))
-    env.SERVICE = serviceName
-    env.HOST_NAME = deployHost
-
-    if (debug == "true"){
-       env.DEBUG_OPTS = service.DEBUG_OPTS
-    }
-    return this
-}
-
 def mvnBuild(java_version="10", serviceName) {
     def server = Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'
     def rtMaven = Artifactory.newMavenBuild()
@@ -35,5 +25,27 @@ def mvnBuild(java_version="10", serviceName) {
     rtMaven.deployer server: server, releaseRepo: 'artifactory', snapshotRepo: 'artifactory'
 
     rtMaven.run pom: "${serviceName}/pom.xml" as String, goals: "-U clean install -Dmaven.test.skip=true -s /home/jenkins/settings.xml", buildInfo: buildInfo
+    return this
+}
+
+def pullArtifact() {
+    def server = Artifactory.newServer url: 'http://artifactory.trph.ru/artifactory', username: 'artifactory', password: 'Ieraipah1thu'
+    def rtMaven = Artifactory.newMavenBuild()
+    buildInfo = Artifactory.newBuildInfo()
+    rtMaven.deployer server: server, releaseRepo: 'artifactory', snapshotRepo: 'artifactory'
+    server.publishBuildInfo buildInfo
+
+    rtMaven.deployer.deployArtifacts buildInfo
+    return this
+}
+
+def setEnv(workspace, serviceName, deployHost) {
+    def project = new XmlSlurper().parse(new File("${workspace}/${serviceName}/pom.xml"))
+    env.SERVICE = serviceName
+    env.HOST_NAME = deployHost
+
+    if (debug == "true"){
+       env.DEBUG_OPTS = service.DEBUG_OPTS
+    }
     return this
 }
