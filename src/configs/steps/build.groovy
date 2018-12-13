b02a96a13299e5e0a1e998a0deea09542b19bcbe
vk <vk@VAK.local>
2018-12-13 10:33:04 +0300

    fixed

--- src/configs/steps/build.groovy
+++ src/configs/steps/build.groovy
@@ -1,14 +1,7 @@
 package config.steps
 
-class Build {
-    def checkoutFrom() {
-        git([url: "git@bitbucket.org:TracePharm/tracepharmservices.git", branch: "master"])
-    }
-    
-    def runArtifactory() {
-        rtMaven.tool = "maven"
-        buildInfo = Artifactory.newBuildInfo()
-        rtMaven.deployer server: server, releaseRepo: 'artifactory', snapshotRepo: 'artifactory'
-    }
+def checkoutFrom() {
+    git([url: "git@bitbucket.org:TracePharm/tracepharmservices.git", branch: "master"])
 }
+    
 return this
