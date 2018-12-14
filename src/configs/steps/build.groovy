299e2a775ec50513d72f94a3dedb6c0ecab80d24
vk <vk@VAK.local>
2018-12-14 11:54:37 +0300

    fixed

--- src/configs/steps/build.groovy
+++ src/configs/steps/build.groovy
@@ -1,5 +1,7 @@
 package config.steps
 
+import groovy.json.JsonSlurper
+
 def checkoutFrom(repo, branch) {
     git([url: "git@bitbucket.org:TracePharm/${repo}", branch: "${branch}"])
 }
@@ -29,18 +31,26 @@ def dockerBuild(serviceName, serviceVersion) {
     sh "docker push docker.trph.ru/${serviceName}:${serviceVersion}"
 }
 
-def deployService(serviceName, serviceVersion, environment, deployStructure, debug) {
-        env.SERVICE = serviceName
-        env.HOST_NAME = deployStructure[environment]['deploy_servers'] 
-        env.STAGE = deployStructure[environment]['stage']
-        env.CPU_LIMIT = deployStructure[environment]['cpu_limit']
-        env.MEMORY_LIMIT = deployStructure[environment]['memory_limit']
-        env.APP_VERSION = "${serviceVersion}"
+def registryRequest(serviceName) {
+    def url = "http://docker.trph.ru/v2/${serviceName}/tags/list"
+    URL tagsUrl = url.toURL()
+    HashMap tagsJson = new JsonSlurper().parse(tagsUrl.newReader())
+    List response = tagsJson.tags
+    return response
+}
 
-        if (debug == "true"){
-           env.DEBUG_OPTS = deployStructure[environment]['debug_opts'] 
-        }
-        sh "erb ${serviceName}/nomad.plan.erb > ${serviceName}/nomad.plan"
-        sh "nomad stop ${serviceName}-${environment} || exit 0"
-        sh "nomad run ${serviceName}/nomad.plan"
+def deployService(serviceName, serviceVersion, environment, deployStructure, debug=false) {
+    env.SERVICE = serviceName
+    env.HOST_NAME = deployStructure[environment]['deploy_servers'] 
+    env.STAGE = deployStructure[environment]['stage']
+    env.CPU_LIMIT = deployStructure[environment]['cpu_limit']
+    env.MEMORY_LIMIT = deployStructure[environment]['memory_limit']
+    env.APP_VERSION = "${serviceVersion}"
+    
+    if (debug == "true"){
+       env.DEBUG_OPTS = deployStructure[environment]['debug_opts'] 
+    }
+    sh "erb ${serviceName}/nomad.plan.erb > ${serviceName}/nomad.plan"
+    sh "nomad stop ${serviceName}-${environment} || exit 0"
+    sh "nomad run ${serviceName}/nomad.plan"
 }
