b698e7558c2ca89b3cf5aae011a1e023be656d44
vk <vk@VAK.local>
2018-12-14 11:12:03 +0300

    fixed

--- src/configs/services/sample.groovy
+++ src/configs/services/sample.groovy
@@ -7,7 +7,7 @@ class Sample {
 
     def deployStructure = [
         dev: [
-          deploy_servers: "dev2",
+          deploy_servers: "dev2.trph.ru",
           stage: "dev",
           debug_opts: "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:18293,suspend=n",
           cpu_limit: "200",
@@ -15,7 +15,7 @@ class Sample {
           jolokia_port: "8083",
         ],
         test: [
-          deploy_servers: "test2",
+          deploy_servers: "test2.trph.ru",
           stage: "test",
           debug_opts: "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:18293,suspend=n",
           cpu_limit: "200",
@@ -23,7 +23,7 @@ class Sample {
           jolokia_port: "8083",
         ],
         stand1: [
-          deploy_servers: "stand1",
+          deploy_servers: "stand1.trph.ru",
           stage: "stand1",
           debug_opts: "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:18293,suspend=n",
           cpu_limit: "200",
@@ -31,7 +31,7 @@ class Sample {
           jolokia_port: "8083",
         ],
         check: [
-          deploy_servers: "check2",
+          deploy_servers: "check2.trph.ru",
           stage: "check",
           debug_opts: "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:18293,suspend=n",
           cpu_limit: "2000",
@@ -39,7 +39,7 @@ class Sample {
           jolokia_port: "8083",
         ],
         prod: [
-          deploy_servers: "prod2",
+          deploy_servers: "prod2.trph.ru",
           stage: "prod",
           debug_opts: "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=0.0.0.0:18293,suspend=n",
           cpu_limit: "2000",
